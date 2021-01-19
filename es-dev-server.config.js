225622791fa461b54fa9e91117c881856533979a
Ben Delarre <delarre@adobe.com>
2021-01-19 13:38:27 -0800

    fix: fixes #79 and updates dependencies

--- es-dev-server.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    appIndex: 'demo/index.html',
-    open: 'demo/index.html',
-    nodeResolve: true,
-    moduleDirs: ['node_modules'],
-};
---
+++ web-dev-server.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    appIndex: 'demo/index.html',
+    open: 'demo/index.html',
+    nodeResolve: true,
+    moduleDirs: ['node_modules'],
+};
